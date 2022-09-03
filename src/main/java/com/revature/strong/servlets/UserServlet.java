19f450b3ebe66a81fad614e3345a8bb961c14ec8
Melissa Clark <melissaclark4@mail.weber.edu>
2022-09-02 22:49:59 -0600

    get all users, sign in needs to be authorized

--- src/main/java/com/revature/strong/servlets/UserServlet.java
+++ src/main/java/com/revature/strong/servlets/UserServlet.java
@@ -2,7 +2,9 @@ package com.revature.strong.servlets;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.revature.strong.dtos.request.NewUserRequest;
+import com.revature.strong.dtos.response.Principal;
 import com.revature.strong.models.User;
+import com.revature.strong.services.TokenService;
 import com.revature.strong.services.UserService;
 import com.revature.strong.utils.custom_exceptions.InvalidRequestException;
 import com.revature.strong.utils.custom_exceptions.ResourceConflictException;
@@ -12,14 +14,17 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.List;
 
 public class UserServlet extends HttpServlet {
     private final ObjectMapper mapper;
     private final UserService userService;
+    private final TokenService tokenService;
 
-    public UserServlet(ObjectMapper mapper, UserService userService) {
-        this.mapper = mapper;
+    public UserServlet(ObjectMapper objectMapper, UserService userService, TokenService tokenService){
+        this.mapper = objectMapper;
         this.userService = userService;
+        this.tokenService = tokenService;
     }
 
     @Override
@@ -45,6 +50,32 @@ public class UserServlet extends HttpServlet {
             resp.getWriter().write(mapper.writeValueAsString(e.getMessage()));
         } catch (ResourceConflictException e) {
             resp.setStatus(409); //conflict
+        } catch (Exception e) {
+            resp.setStatus(404);
+        }
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String token = req.getHeader("Authorization");
+        Principal principal = tokenService.extractRequesterDetails(token);
+
+        try {
+            if(principal.getCoach().equals(true)) {
+                String username = req.getParameter("username");
+
+                resp.setContentType("application/json");
+                if (username != null){
+                    resp.getWriter().write(mapper.writeValueAsString(userService.getUserByUsername(username)));
+                } else {
+                    List<User> userList = userService.getAllUsers();
+                    resp.getWriter().write(mapper.writeValueAsString(userList));
+                }
+            } else {
+                resp.setStatus(403);//forbidden
+            }
+        } catch (NullPointerException e) {
+            resp.setStatus(401);
         }
     }
 }
